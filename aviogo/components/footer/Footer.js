9dd1221978774d33f6be4c3bee5488f0578f9ab6
Knowone27 <63480988+Knowone27@users.noreply.github.com>
2023-10-29 10:51:15 -0400

    Update Footer.js
    quick fix hopefully

--- aviogo/components/footer/Footer.js
+++ aviogo/components/footer/Footer.js
@@ -2,7 +2,7 @@ import styles from './footer.module.css'
 const Footer = () => {
   return (
     <div className={styles.container}>
-        <img src="./aviogo_logo.png"  alt="logo" className={styles.logo}/>
+        <image src="./aviogo_logo.png"  alt="logo" className={styles.logo}/>
         <p>
           Soaring above the cloud (computing). Remember, in our skies, turbulence is just another word for fun!
         </p>
